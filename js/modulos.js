8c59cb4fffd2087d25586b4aa67b8fc5f989321a
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2025-03-26 23:34:37 -0300

    Update modulos.js

---
+++ js/modulos.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  document.querySelector("nav").innerHTML = `
+    <img>
+    <div>
+      <ul>
+        <li><a href="#">opcion</a></li>
+        <li><a href="#">opcion</a></li>
+        <li><a href="#">opcion</a></li>
+      </ul>
+      <a href="#" class="ordenar"></a>
+    </div>
+  `;
+
+  document.querySelector("footer").innerHTML = `
+    <p></p>
+    <div class="redes">
+      <a href="#"><img></a>
+      <a href="#"><img></a>
+      <a href="#"><img></a>
+      <a href="#"><img></a>
+    </div>
+  `;
+  
+})

5ad3db3358fc7e535f1ad99957fc4b7207e572fe
WalterDevIn <martinez.walter.mail@gmail.com>
2025-04-13 12:22:46 -0300

    Primera correcion

--- js/modulos.js
+++ js/modulos.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   document.querySelector("nav").innerHTML = `
-    <img>
+    <img src="#" alt="Logo">
     <div>
       <ul>
         <li><a href="index.html">inicio</a></li>
         <li><a href="pizzas.html">pizzas</a></li>
         <li><a href="contacto.html">contacto</a></li>
       </ul>
-      <a href="#" class="ordenar"></a>
+      <a href="#" class="ordenar">Boton Ordenar</a>
     </div>
   `;
 
   document.querySelector("footer").innerHTML = `
-    <p></p>
+    <p>Copyright(c) 2025 y demas</p>
     <div class="redes">
-      <a href="#"><img></a>
-      <a href="#"><img></a>
-      <a href="#"><img></a>
-      <a href="#"><img></a>
+      <a href="#"><i class="fa-solid fa-instagram"></i></a>
+      <a href="#"><i class="fa-solid fa-facebook"></i></a>
+      <a href="#"><i class="fa-solid fa-x-twitter"></i></a>
+      <a href="#"><i class="fa-solid fa-envelope"></i></a>
     </div>
   `;
   

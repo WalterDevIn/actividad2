5d1aee9123f94f9c6565859a44402589e2dfad8d
winter <martinez.walter@tecnica7.edu.ar>
2025-05-07 20:21:22 -0300

    Contacto Responsivo

--- js/modulos.js
+++ js/modulos.js
@@ -23,9 +23,9 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("footer").innerHTML = `
     <p>Footer | Copyright(c) 2025</p>
     <div class="redes">
-      <a href="#"><i class="fa-solid fa-instagram"></i></a>
-      <a href="#"><i class="fa-solid fa-facebook"></i></a>
-      <a href="#"><i class="fa-solid fa-x-twitter"></i></a>
+      <a href="#"><i class="fa-brands fa-instagram"></i></a>
+      <a href="#"><i class="fa-brands fa-facebook"></i></a>
+      <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
       <a href="#"><i class="fa-solid fa-envelope"></i></a>
     </div>
   `;
